bf9300e79725e5a9ff54a7d78dd3541bcf59c020
karanbawa <karan.bawa2@gmail.com>
2023-11-04 13:37:13 +0530

    fix: lint errors

--- app/components/pages/about-us/AboutTextBlock.js
+++ app/components/pages/about-us/AboutTextBlock.js
@@ -30,29 +30,14 @@ const AboutTextBlock = () => {
         <div className="about_content">
           <h2 className="title">Welcome To The Carportal</h2>
           <p className="mb30">
-            Established in 2022, Carportal stands as the beacon of automotive
-            innovation in India. Born from the visionary minds at Unicorn Tech
-            Media, we&apos;re more than just an automotive website—we&apos;re a movement.
+           </p>
+          <p className="mb30">
+
           </p>
           <p className="mb30">
-            Our platform is crafted by industry veterans, merging passion with
-            expertise. Dive deep into our comprehensive car reviews, immersive
-            3D showcases, and real-time updates, ensuring you&apos;re always ahead of
-            the curve.
           </p>
           <p className="mb30">
-            We don&apos;t just inform; we collaborate. With strong ties to top
-            manufacturers and dealerships, we&apos;re reshaping the car buying and
-            selling experience. And with our sister platforms delving into Tyres
-            and Services, Carportal is driving the future of automotive
-            digitalization.
-          </p>
-          <p className="mb30">
-            Collaboration lies at the heart of Carportal&apos;s ethos. We maintain a
-            synergistic relationship with automotive manufacturers and
-            dealerships, assisting them with cutting-edge IT solutions that
-            optimize business performance and elevate conversion rates.
-          </p>
+            </p>
           <p className="mb30">
           Come aboard Carportal—the future of automobiles is just a click away.
           </p>
