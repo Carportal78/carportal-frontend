1a94aae9ccfea8642ba9ee89e9b67e6bb6fb208a
karanbawa <karan.bawa2@gmail.com>
2024-01-13 18:49:28 +0530

    code push

--- app/(blog)/blog/[blogName]/[id]/page.js
+++ app/(blog)/blog/[blogName]/[id]/page.js
@@ -1,21 +1,21 @@
 "use client";
-import Footer from "@/app/components/common/Footer";
-import DefaultHeader from "@/app/components/common/DefaultHeader";
-import HeaderSidebar from "@/app/components/common/HeaderSidebar";
-import HeaderTop from "@/app/components/common/HeaderTop";
-import MobileMenu from "@/app/components/common/MobileMenu";
-import LoginSignupModal from "@/app/components/common/login-signup";
+import Footer from "../../../../components/common/Footer";
+import DefaultHeader from "../../../../components/common/DefaultHeader";
+import HeaderSidebar from "../../../../components/common/HeaderSidebar";
+import HeaderTop from "../../../../components/common/HeaderTop";
+import MobileMenu from "../../../../components/common/MobileMenu";
+import LoginSignupModal from "../../../../components/common/login-signup";
 import Image from "next/image";
-import Blog from "@/app/components/common/Blog";
-import Meta from "@/app/components/blog/blog-single/Meta";
-import Blockquote from "@/app/components/blog/blog-single/Blockquote";
-import Features from "@/app/components/blog/blog-single/Features";
-import Requirements from "@/app/components/blog/blog-single/Requirements";
-import Share from "@/app/components/blog/blog-single/Share";
-import Tag from "@/app/components/blog/blog-single/Tag";
-import Pagination from "@/app/components/blog/blog-single/Pagination";
-import Comments from "@/app/components/blog/blog-single/Comments";
-import CommentForm from "@/app/components/blog/blog-single/CommentForm";
+import Blog from "../../../../components/common/Blog";
+import Meta from "../../../../components/blog/blog-single/Meta";
+import Blockquote from "../../../../components/blog/blog-single/Blockquote";
+import Features from "../../../../components/blog/blog-single/Features";
+import Requirements from "../../../../components/blog/blog-single/Requirements";
+import Share from "../../../../components/blog/blog-single/Share";
+import Tag from "../../../../components/blog/blog-single/Tag";
+import Pagination from "../../../../components/blog/blog-single/Pagination";
+import Comments from "../../../../components/blog/blog-single/Comments";
+import CommentForm from "../../../../components/blog/blog-single/CommentForm";
 import { useEffect, useState } from "react";
 
 const metadata = {
