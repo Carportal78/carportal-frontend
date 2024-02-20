85aa88d4e9f6673bd29afe265c5f5f176e235963
karanbawa <karan.bawa2@gmail.com>
2024-02-21 02:08:10 +0530

    fix: code push - fixed variant pricing

--- app/components/common/contactdealerForm/ExShowroomPriceForm.js
+++ app/components/common/contactdealerForm/ExShowroomPriceForm.js
@@ -1,72 +1,73 @@
-import React from "react";
-import ContactDealer from "./ContactDealer";
-import SignupForm from "./SignupForm";
-import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Select from 'react-select';
+import statesCitiesList from '../../../../public/jsondata/state-and-city.json';
+import { useAtom } from "jotai";
+import { selectCityAtom } from "../../atoms/categoriesAtoms";
 
 const ExShowroomPriceForm = ({ carModelDetails }) => {
+    const [selectedCity, setSelectedCity] = useState(null);
+    const [city, selectCityData] = useAtom(selectCityAtom);
+    const [cityOptions, setCityOptions] = useState([]);
+
+    useEffect(() => {
+        const allCities = [];
+        Object.keys(statesCitiesList).forEach(state => {
+            statesCitiesList[state].forEach(city => {
+                allCities.push({
+                    value: city.id,
+                    label: city.city
+                });
+            });
+        });
+        setCityOptions(allCities);
+    }, []);
+
+    const handleCityChange = (selectedOption) => {
+        selectCityData(selectedOption);
+        setSelectedCity(selectedOption);
+    };
+
     return (
         <div className="modal-dialog modal-dialog-centered">
             <div className="modal-content">
-                {/* End Modal close button */}
-
+                <div className="modal-header">
+                    <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    />
+                </div>
                 <div className="modal-body container p60">
                     <div className="row">
                         <div className="col-lg-12">
-                            <ul
-                                className="sign_up_tab nav nav-tabs"
-                                id="myTab"
-                                role="tablist"
-                            >
+                            <ul className="sign_up_tab nav nav-tabs" id="myTab" role="tablist">
                                 <li className="nav-item">
-                                    <a
-                                        className={`nav-link active`}
-                                        id={`1-tab`}
-                                        data-bs-toggle="tab"
-                                        href={`#1`}
-                                        role="tab"
-                                        aria-controls={1}
-                                        aria-selected={true}
-                                    >
-                                        City List
-                                    </a>
+                                    <a className="nav-link active" id="select-city-tab" data-bs-toggle="tab" href="#select-city" role="tab" aria-controls="select-city" aria-selected="true">Select City</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    {/* End .row */}
-
                     <div className="tab-content container p0" id="myTabContent">
-                        <div
-                            className={`row mt30 tab-pane fade show active`}
-                            id={1}
-                            role="tabpanel"
-                            aria-labelledby={`1-tab`}
-                            key={1}
-                        >
+                        <div className="row mt30 tab-pane fade show active" id="select-city" role="tabpanel" aria-labelledby="select-city-tab" key="select-city">
                             <div className="col-lg-12">
                                 <div className="login_form">
-                                    <p>
-                                        We need your city to customize your experience
-                                    </p>
-                                    <form>
-                                        {/* Car Brand */}
-                                        {/* <div className="mb-2">
-                                            <label className="form-label">Car Brand</label>
-                                            <select name="carBrand" className="form-control" onChange={handleChange} value={formData.carBrand}>
-                                                <option value="">Select Brand</option>
-                                                {carBrands?.map(brand => <option key={brand._id} value={brand._id}>{brand.brandName}</option>)}
-                                            </select>
-                                        </div> */}
-                                    </form>
+                                    <p>We need your city to customize your experience</p>
+                                    <Select
+                                        placeholder="Select City..."
+                                        value={selectedCity}
+                                        onChange={handleCityChange}
+                                        options={cityOptions}
+                                        className="select2-selection"
+                                    />
                                 </div>
                             </div>
                         </div>
                     </div>
-                    {/* End tab-content */}
                 </div>
-                {/* End modal-body */}
             </div>
         </div>
-    )};
+    );
+};
 
 export default ExShowroomPriceForm;
