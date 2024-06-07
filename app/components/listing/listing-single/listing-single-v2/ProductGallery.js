4c6506b5e40f512e486e701670dd0efd259064a4
karanbawa <karan.bawa2@gmail.com>
2024-06-07 23:01:36 +0530

    fix: fixed the variant features details

--- app/components/listing/listing-single/listing-single-v2/ProductGallery.js
+++ app/components/listing/listing-single/listing-single-v2/ProductGallery.js
@@ -106,9 +106,9 @@ function OffCanvasExampleCompare({ carVariantsList, name, relatedCars, carModelD
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const currentCarDetails = compareCars.find(car => car?.models?.modelId === carModelDetails?._id)
-  const filterCurrentCarInCompareCar = compareCars.filter(car => car?.models?.modelId !== currentCarDetails?.models?.modelId)
-  const renderCompareCarsList = filterCurrentCarInCompareCar?.length > 10 ? filterCurrentCarInCompareCar.slice(0,10) : filterCurrentCarInCompareCar;
+  const currentCarDetails = compareCars?.find(car => car?.models?.modelId === carModelDetails?._id)
+  const filterCurrentCarInCompareCar = compareCars?.filter(car => car?.models?.modelId !== currentCarDetails?.models?.modelId)
+  const renderCompareCarsList = filterCurrentCarInCompareCar?.length > 10 ? filterCurrentCarInCompareCar?.slice(0,10) : filterCurrentCarInCompareCar;
 
   return (
     <>
