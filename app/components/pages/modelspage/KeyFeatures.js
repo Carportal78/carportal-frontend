85041f9caa22ef0082790dd45fa34249abbe8d90
karanbawa <karan.bawa2@gmail.com>
2024-09-05 16:29:51 +0530

    fix: code push

--- app/components/pages/modelspage/KeyFeatures.js
+++ app/components/pages/modelspage/KeyFeatures.js
@@ -5,19 +5,32 @@ const KeyFeatures = ({ features, modelName }) => {
   return (
     <>
      <h4>Key Features of {modelName}</h4>
-     <div className="flex overflow-x-auto whitespace-nowrap">
-      {features?.map((feature, index) => (
-        <div key={index} className="inline-block flex-shrink-0 mr-4 w-[100px]">
-          <Image 
-            width={100} 
-            height={100} 
-            src={feature?.image?.url} 
-            alt={feature?.image?.altText} 
-            className="w-full h-auto"
-          />
-          <p className="mt-2 text-sm whitespace-normal">{feature.featureDescription}</p>
-        </div>
-      ))}
+     <div className="container-fluid px-0">
+      <div className="row flex-nowrap overflow-auto gap-2">
+        {features?.map((feature, index) => (
+          <div key={index} className="col-4 col-md-3 col-lg-2 px-2">
+            <div className="text-center">
+              <Image 
+                width={100} 
+                height={100} 
+                src={feature?.image?.url} 
+                alt={feature?.image?.altText} 
+                className="img-fluid mb-2"
+              />
+              <p className="small text-break mb-0">{feature.featureDescription}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        .row::-webkit-scrollbar {
+          display: none;
+        }
+        .row {
+          -ms-overflow-style: none;
+          scrollbar-width: none;
+        }
+      `}</style>
     </div>
     </>
   );
